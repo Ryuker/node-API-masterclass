ca51f929d9b8666b4a8499611aba7407cf8dd6a5
ryuker <lifeisagamenotaproduct@gmail.com>
2024-03-28 17:46:34 +0100

    removed console log

--- devcamper_api/controllers/bootcamps.js
+++ devcamper_api/controllers/bootcamps.js
@@ -19,8 +19,6 @@ exports.getBootcamps = asyncHandler( async(req, res, next ) => {
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
 
-  console.log(reqQuery);
-
   // Create query string
   let queryStr = JSON.stringify(reqQuery);
   
